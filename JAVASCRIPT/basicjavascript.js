97d5dee8e9db5fbf21f579ecb14339f21a5eae05
Bashu Tiwari <bashutiwariapsg22@gmail.com>
2021-07-10 23:47:46 +0530

    Learning JavaScript.

--- JAVASCRIPT/basicjavascript.js
+++ JAVASCRIPT/basicjavascript.js
@@ -1,2 +1,83 @@
-document.write("<h1>Hello World</h1>");
-document.write("<p>My name is Bahsu Tiwari</p>");
+// console.log("Hello World");
+// console.log("My name is Bahsu Tiwari");
+
+// const array1 = [1,4,9,16,25] ;
+
+// let newArr1 = array1.map((currVal,index,array1) => {
+//     return currVal>9;
+// });
+
+// let newArr2 = array1.filter((currVal,index,array1) => {
+//     return currVal>9;
+// });
+
+// console.log(array1);
+// console.log(newArr1);
+// console.log(newArr2);
+
+// let arr = [16,25,64,100,125];
+
+// let newArr = arr.forEach((currElement,index,arr) => {
+//     return 'Element ${currElement} is located on Index no ${index} of the array ${arr}'
+// });
+
+// console.log(newArr);
+
+
+//    let a = 22;
+//    let b = 44;
+//    function add(a, b) {
+//       return a + b;
+//    }
+
+//    console.log("The sum of " + a + "and " + b + " = is " + add(a,b));
+
+
+// let arr = [1 , 2 , 3 , 4 , 5];
+
+// let newArr = arr.map((currVal) => {
+//     return currVal*2 ;
+// }).filter((currVal) => {
+//     return currVal>5 ;
+// });
+
+// console.log(newArr); 
+
+// let arr = [1,2,3,4,5];
+
+// let sum = arr.reduce((accumulator,currVal,index,arr) => {
+//     debugger;
+//     return accumulator+=currVal ;
+// });
+
+// console.log(sum);
+
+// let myName = "Bashu Tiwari";
+// console.log(myName.length);
+
+
+// Date And Time Object 
+
+// const d = new Date();
+// console.log(d.getDate());
+
+// function harry(){
+//     console.log('This is the best person');
+// }
+
+// const harry = () => {
+//     console.log('this is the best person')
+// }
+
+
+// console.log(harry);
+
+// const greet = ()=> {'Good Morning'} ;
+
+
+// console.log(greet());
+
+let addition = function(x,y) {
+    return x + y;
+   }
+   console.log(addition(10,10));
